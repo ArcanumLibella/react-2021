8f070f77691aa5d86caaa00d90c5076cfb498eb9
ArcanumLibella <fiona.roux@hetic.net>
2021-03-05 16:05:48 +0100

    feat: add fontAwesome library and some icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+
 import Header from './components/Header';
 import List from './components/List';
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone, faEnvelope, faSortDown } from '@fortawesome/free-solid-svg-icons'
+
 
 export default class Contact extends Component {
   render() {
@@ -6,14 +10,23 @@ export default class Contact extends Component {
       <div className="card-contact m-4 p-4 shadow-md bg-indigo-100 rounded">
         <h4 className="uppercase font-bold mb-2">
           {this.props.nom}
+          <FontAwesomeIcon icon={faSortDown} className="ml-2"/>
         </h4>
 
         <ul className="mb-4">
           <li className="">
-            <span className="font-medium">Téléphone : </span>{this.props.tel}
+            <span className="font-medium">
+              <FontAwesomeIcon icon={faPhone} className="mr-2"/>
+              Téléphone :
+            </span>
+            {this.props.tel}
           </li>
           <li className="">
-            <span className="font-medium">Email : </span>{this.props.email}
+            <span className="font-medium">
+              <FontAwesomeIcon icon={faEnvelope}  className="mr-2"/>
+              Email : 
+            </span>
+            {this.props.email}
           </li>
         </ul>
       </div>
